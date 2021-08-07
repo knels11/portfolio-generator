25ff4e3ecc717b55495e3d56f4e29ae44ef63259
kiehp <kierra.nelson@aol.com>
2021-08-07 07:37:53 -0400

    added src folder to export modules

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 //template literals
 //const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+const fs = require('fs');
+//receive exported functions- the object in module.exports assignment will be reassigned to generatePage variable
+const generatePage = require('./src/page-template.js');
 
 //profile data args array- holds the user command line args
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-//html template literal/string
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE HTML>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title> Portfolio Demo </title>
-    </head>
-    
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+const profileDataArgs = process.argv.slice(2);
 //es6 ft ~ assignment destructuring: assigns elements of an array to a variable in a single expression
 const [name, github] = profileDataArgs;
 
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    //in the callback function block, a conditional statement checks for the err being returned
+    if (err) throw new Error(err);
+    //success statement that directs users to inspect the newly created file
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
 //logs name and github inputs and confirm they match
-console.log(name,github);
-console.log(generatePage(name, github));
+//console.log(name,github);
+//console.log(generatePage(name, github));
 
 //multi line strings
 //const generatePage = (userName, githubName) => {
